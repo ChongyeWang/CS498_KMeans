3b980852aacc80746d89653b93c9af7029c45d0f
Chongye jWang <wangchongye@126.com>
2018-10-06 20:34:37 -0500

    Updated

--- hw5.py
+++ hw5.py
@@ -2,6 +2,7 @@ __author__ = "Chongye Wang, Si Chen, Pan Zhang"
 
 import os
 import numpy as np
+from sklearn.cluster import KMeans
 
 
 def divid(action_dict, length):
@@ -10,7 +11,7 @@ def divid(action_dict, length):
         new_action_dict[action] = []
         for list in action_dict[action]:
             row_size = len(list) # The number of rows
-            num_of_blocks = int(row_size / (length * 3))
+            num_of_blocks = int(row_size / length)
             if num_of_blocks > 0: # Make sure there is at least one block of size length * 3
                 for idx in range(0, num_of_blocks):
                     new_list = [] # size of (1, length * 3)
@@ -69,8 +70,23 @@ for folder in selected_folders:
 action_dict = divid(action_dict, 32)
 label_action = reverse_dict(action_dict)
 
+int_name = {}
+
+
 sum_all_list = []
 for action in action_dict:
     for list in action_dict[action]:
         sum_all_list.append(list)
 sum_all_list = np.array(sum_all_list)
+
+
+#Cluster with KMeans
+kmeans = KMeans(n_clusters=2, random_state=0).fit(sum_all_list)
+predict_result = kmeans.labels_
+
+
+#Test
+num_correct = 0
+for idx in range(len(sum_all_list)):
+    preidct_result = predict_result[idx]
+    estimated_result = predict_result[idx]
