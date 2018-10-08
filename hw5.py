309d8e39c2bc407ddc3ad742983ff7eeb046c3a3
Chongye jWang <wangchongye@126.com>
2018-10-07 19:32:16 -0500

    Updated

--- hw5.py
+++ hw5.py
@@ -3,7 +3,7 @@ __author__ = "Chongye Wang, Si Chen, Pan Zhang"
 import os
 import numpy as np
 from sklearn.cluster import KMeans
-
+import sys
 
 def divid(action_dict, length):
     new_action_dict = {}
@@ -12,18 +12,16 @@ def divid(action_dict, length):
         for list in action_dict[action]:
             row_size = len(list) # The number of rows
             num_of_blocks = int(row_size / length)
-            if num_of_blocks > 0: # Make sure there is at least one block of size length * 3
+            if num_of_blocks > 0: # Make sure there is at least one block
                 for idx in range(0, num_of_blocks):
-                    new_list = [] # size of (1, length * 3)
+                    new_list = []
                     start_index = idx * length
                     end_index = (idx + 1) * length
                     for l in range(start_index, end_index):
                         new_list += list[l]
-
                     new_action_dict[action].append(new_list)
-                    print(new_action_dict)
-    print(new_action_dict)
     return new_action_dict
+
 
 def reverse_dict(action_dict):
     result = {}
@@ -33,7 +31,7 @@ def reverse_dict(action_dict):
             result[tuple(l)] = key
     return result
 
-
+############################## Data preprocessing ##############################
 
 # Get the current working directory
 curr_directory = os.getcwd()
@@ -45,14 +43,9 @@ all_folders = [d for d in os.listdir(all_data) if os.path.isdir(os.path.join(all
 # Get only folders without 'MODEL'
 selected_folders = [d for d in all_folders if 'MODEL' not in d]
 
-
-data = []
-
 action_dict = {}
 
-
 for folder in selected_folders:
-    print(folder)
     action_dict[folder] = []
     one_folder = all_data + '/' + folder
     all_files = os.listdir(one_folder)# All files in one folder
@@ -60,19 +53,23 @@ for folder in selected_folders:
         file_path = one_folder + '/' + file
         with open(file_path, 'r') as f:
             lines = f.readlines()
-        lines = [x.strip() for x in lines] #
+        lines = [x.strip() for x in lines]
         for idx in range(len(lines)):
             lines[idx] = lines[idx].split(' ')
             lines[idx] = [int(x) for x in lines[idx]]
         action_dict[folder].append(lines)
 
 
+################################################################################
+
+#we do vector quantization
 action_dict = divid(action_dict, 32)
+
+# reverse the key and value of action_dict
 label_action = reverse_dict(action_dict)
 
-int_name = {}
 
-
+#Sum all the list
 sum_all_list = []
 for action in action_dict:
     for list in action_dict[action]:
@@ -80,13 +77,46 @@ for action in action_dict:
 sum_all_list = np.array(sum_all_list)
 
 
-#Cluster with KMeans
-kmeans = KMeans(n_clusters=2, random_state=0).fit(sum_all_list)
+#Cluster with KMeans(we use 14 clusters here)
+kmeans = KMeans(n_clusters=14, random_state=0).fit(sum_all_list)
 predict_result = kmeans.labels_
 
 
-#Test
-num_correct = 0
-for idx in range(len(sum_all_list)):
-    preidct_result = predict_result[idx]
-    estimated_result = predict_result[idx]
+# Count the number of real actions
+action_count = {}
+for action in action_dict:
+    action_count[action] = 0
+for action in action_dict:
+    action_count[action] = len(action_dict[action])
+print(action_count)
+
+predicted_count = {}
+for idx in range(14):
+    predicted_count[idx] = 0
+for prediction in predict_result:
+    predicted_count[prediction] += 1
+print(predicted_count)
+
+"""
+{'Liedown_bed': 345, 'Walk': 2835, 'Eat_soup': 207, 'Getup_bed': 1381, 'Descend_stairs': 460,
+'Use_telephone': 470, 'Standup_chair': 745, 'Brush_teeth': 926, 'Climb_stairs': 1210,
+'Sitdown_chair': 727, 'Eat_meat': 974, 'Comb_hair': 722, 'Drink_glass': 1288, 'Pour_water': 1254}
+
+{0: 907, 1: 2587, 2: 384, 3: 1747, 4: 724, 5: 543, 6: 486, 7: 1068, 8: 489,
+9: 625, 10: 609, 11: 736, 12: 2488, 13: 151}
+"""
+
+action_cluster = {}
+
+for prediction in predicted_count:
+    prediction_count = predicted_count[prediction]
+    corresponding_result = ''
+    curr_min = sys.maxsize
+    for action in action_count:
+        real_count = action_count[action]
+        difference = abs(prediction_count - real_count)
+        if difference < curr_min:
+            curr_min = difference
+            corresponding_result = action
+    action_cluster[prediction] = corresponding_result
+print(action_cluster)
